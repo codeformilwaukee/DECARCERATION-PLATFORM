52347dadae30ff47d22ad0205a1e54043490af3d
bart1259 <bsrt1259@gmail.com>
2019-10-16 21:28:58 -0500

    Adding rudementary landing page

--- web-app/src/components/LandingPage/index.js
+++ web-app/src/components/LandingPage/index.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import Paper from '@material-ui/core/Paper';
+import Image from 'react-image-resizer';
 
 const LandingPage = () => (
-    <Paper>
-        <p>Stuffffff</p>
-    </Paper>
+    
+    
+    <div>
+        <div className = "welcomeWrapper">
+            
+        <img src={"images/b.png"}
+         resizeMode={'cover'}
+         stretched = {false}
+          style={{ width: '100%', height: '20%' }} />
+        </div>
+            
+    </div>
 );
 
 export default LandingPage;
