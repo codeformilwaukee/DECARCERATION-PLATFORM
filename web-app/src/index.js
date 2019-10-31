12df16996ab85c2d8967c27519156d82e9dc76d1
david <david.kaftan@marquetteenergyanalytics.com>
2019-10-30 20:21:46 -0500

    Added login page

--- web-app/src/index.js
+++ web-app/src/index.js
@@ -3,6 +3,18 @@ import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 import App from './components/App';
 import './index.css';
+import Amplify from 'aws-amplify';
+import config from './config';
+
+Amplify.configure({
+    Auth: {
+      mandatorySignIn: true,
+      region: config.cognito.REGION,
+      userPoolId: config.cognito.USER_POOL_ID,
+      identityPoolId: config.cognito.IDENTITY_POOL_ID,
+      userPoolWebClientId: config.cognito.APP_CLIENT_ID
+    }
+  });
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
