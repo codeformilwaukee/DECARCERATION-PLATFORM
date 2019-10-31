9672b9a61cb6bd8d47b1810d2f1b919e234c7896
david <david.kaftan@marquetteenergyanalytics.com>
2019-10-30 20:34:31 -0500

    adding the config because I think its safe to publish

---
+++ web-app/src/config.js
@@ -0,0 +1,8 @@
+export default {
+    cognito: {
+        REGION: "us-east-1", // these are my (David's) defaults. I need access to the aws account to set these up.
+        USER_POOL_ID: "us-east-1_PjDumYdC0",
+        APP_CLIENT_ID: "6bh1003bp2movlu9lp34bfs85e",
+        IDENTITY_POOL_ID: "6bh1003bp2movlu9lp34bfs85e"
+      }
+}
