61241f41a3c2fa5daaea29045126cedeb70c1441
Paul Rinaldi <rinaldip@msoe.edu>
2020-01-25 13:16:04 -0600

    centered calendar, added some mock data

--- web-app/src/components/CalendarPage/index.js
+++ web-app/src/components/CalendarPage/index.js
@@ -7,23 +7,35 @@ const localizer = momentLocalizer(moment);
 const CalendarPage = () => {
   const myEvents = [
       {
-      title: 'Surprise Party',
-      start: new Date('October 18, 2019 03:24:00'),
-      end: new Date('October 19, 2019 03:24:00'),
+      title: 'major hacks 1',
+      start: new Date('January 1, 2020 08:24:00'),
+      end: new Date('January 7, 2020 12:24:00'),
+    },
+    {
+      title: 'major hacks 2',
+      start: new Date('January 8, 2020 08:24:00'),
+      end: new Date('January 25, 2020 12:24:00'),
+    },
+    {
+      title: 'major hacks 3',
+      start: new Date('January 26, 2020 08:24:00'),
+      end: new Date('February 4, 2020 12:24:00'),
     }
   ];
-  
+
   return (
-    <div>
-      <p>
-      Welcome to the calendar!
-      </p>
+    <div style={{
+      position: 'absolute', 
+      left: '50%', 
+      top: '50%',
+      transform: 'translate(-50%, -50%)',
+      marginTop: "200px"}}>
       <Calendar
         localizer={localizer}
         events={myEvents}
         startAccessor="start"
         endAccessor="end"
-        style={{ height: 500 }}
+        style={{height: "800px", width: "1200px"}}
       />
     </div>
 );
