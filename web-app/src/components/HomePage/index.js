09a165546f749db5bbc6964c3aa0652f21344c23
Helen D Denisenko <helen.denisenko@cbre.com>
2020-06-10 11:23:12 -0400

    add external links to home page

--- web-app/src/components/HomePage/index.js
+++ web-app/src/components/HomePage/index.js
@@ -1,7 +1,39 @@
 import React from 'react';
+import './HomePage.css'
+import { Typography } from '@material-ui/core';
 
 const HomePage = () => (
-    <div></div>
+    <div className="external-links">
+      <Typography style = {{
+          color: 'black',
+          fontSize: '1.5vw',
+          marginTop: '20px'}}>
+          External Links
+      </Typography>
+      <ul>
+        <li>
+          <a href="https://211wisconsin.communityos.org/guided-search">
+            <Typography style = {{
+                color: 'black',
+                fontSize: '1vw',
+                textDecoration: 'underline',
+                marginTop: '20px'}}>
+                211 Madison Basic Needs Search
+            </Typography>
+          </a>
+        </li>
+        <li>
+          <a href="https://www.mkehometostay.org/">
+            <Typography style = {{
+                color: 'black',
+                fontSize: '1vw',
+                textDecoration: 'underline'}}>
+                Home to Stay
+            </Typography>
+          </a>
+        </li>
+      </ul>
+    </div>
 );
 
 export default HomePage;
