e46bf3d08c216cb1824ab9b2163e078b8182213e
Paul Rinaldi <rinaldi.paul1@gmail.com>
2020-06-10 21:56:50 -0500

    removes li to clean up layout

--- web-app/src/components/HomePage/index.js
+++ web-app/src/components/HomePage/index.js
@@ -1,39 +1,44 @@
-import React from 'react';
-import './HomePage.css'
-import { Typography } from '@material-ui/core';
+import React from "react";
+import "./HomePage.css";
+import { Typography } from "@material-ui/core";
 
 const HomePage = () => (
-    <div className="external-links">
-      <Typography style = {{
-          color: 'black',
-          fontSize: '1.5vw',
-          marginTop: '20px'}}>
-          External Links
-      </Typography>
-      <ul>
-        <li>
-          <a href="https://211wisconsin.communityos.org/guided-search">
-            <Typography style = {{
-                color: 'black',
-                fontSize: '1vw',
-                textDecoration: 'underline',
-                marginTop: '20px'}}>
-                211 Madison Basic Needs Search
-            </Typography>
-          </a>
-        </li>
-        <li>
-          <a href="https://www.mkehometostay.org/">
-            <Typography style = {{
-                color: 'black',
-                fontSize: '1vw',
-                textDecoration: 'underline'}}>
-                Home to Stay
-            </Typography>
-          </a>
-        </li>
-      </ul>
-    </div>
+  <div className="external-links">
+    <Typography
+      style={{
+        color: "black",
+        fontSize: "1.5vw",
+        marginTop: "20px",
+      }}
+    >
+      External Links
+    </Typography>
+    <ul>
+      <a href="https://211wisconsin.communityos.org/guided-search">
+        <Typography
+          style={{
+            color: "black",
+            fontSize: "2vw",
+            textDecoration: "underline",
+            marginTop: "20px",
+          }}
+        >
+          211 Madison Basic Needs Search
+        </Typography>
+      </a>
+      <a href="https://www.mkehometostay.org/">
+        <Typography
+          style={{
+            color: "black",
+            fontSize: "2vw",
+            textDecoration: "underline",
+          }}
+        >
+          Home to Stay
+        </Typography>
+      </a>
+    </ul>
+  </div>
 );
 
 export default HomePage;
