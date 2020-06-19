d275c0c95b181d1190cc1f7ba15aca8033fb0a30
Andrew Terranova <aterrano@adobe.com>
2020-06-19 13:00:00 -0500

    Adds JSDoc comments

--- web-app/src/components/Footer/PrivacyPolicy.js
+++ web-app/src/components/Footer/PrivacyPolicy.js
@@ -3,6 +3,9 @@ import { Link } from 'react-router-dom';
 
 import * as ROUTES from '../../constants/routes';
 
+/**
+ * The PrivacyPolicy component provides a link to the Privacy Policy Page.
+ */
 const PrivacyPolicy = () => (
     <Link to={ROUTES.PRIVACY_POLICY}>
         Privacy Policy
--- web-app/src/components/Footer/TermsAndConditions.js
+++ web-app/src/components/Footer/TermsAndConditions.js
@@ -3,6 +3,9 @@ import { Link } from 'react-router-dom';
 
 import * as ROUTES from '../../constants/routes';
 
+/**
+ * The TermsAndConditions component provides a link to the Terms and Conditions Page.
+ */
 const TermsAndConditions = () => (
     <Link to={ROUTES.TERMS_AND_CONDITIONS}>
         Terms and Conditions
