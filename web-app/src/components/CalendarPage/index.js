4994db53cb7079bbb4b597b27a9ba976bd3e5c5c
Gordon Caister <gordoncaister@gmail.com>
2020-07-05 11:34:57 -0500

    fixed issue 64 for real

--- web-app/src/components/CalendarPage/index.js
+++ web-app/src/components/CalendarPage/index.js
@@ -6,38 +6,38 @@ import NewCalendarEntryModal from './NewCalendarEntryModal';
 import ExistingCalendarEntryModal from './ExistingCalendarEntryModal';
 
 const localizer = momentLocalizer(moment);
+//initialData
+const myEvents = [
+  {
+  title: 'major hacks 1',
+  start: new Date('January 1, 2020 08:24:00'),
+  end: new Date('January 7, 2020 12:24:00'),
+},
+{
+  title: 'major hacks 2',
+  start: new Date('January 8, 2020 08:24:00'),
+  end: new Date('January 25, 2020 12:24:00'),
+},
+{
+  title: 'major hacks 3',
+  start: new Date('January 26, 2020 08:24:00'),
+  end: new Date('February 4, 2020 12:24:00'),
+}
+];
 
+//initialstate
+
+const initialCurrEvent = {
+title: "",
+start: "",
+end: "",
+repeat: false,
+description: "",
+location: "",
+}
 const CalendarPage = () => {
 
-  //initialData
-  const myEvents = [
-      {
-      title: 'major hacks 1',
-      start: new Date('January 1, 2020 08:24:00'),
-      end: new Date('January 7, 2020 12:24:00'),
-    },
-    {
-      title: 'major hacks 2',
-      start: new Date('January 8, 2020 08:24:00'),
-      end: new Date('January 25, 2020 12:24:00'),
-    },
-    {
-      title: 'major hacks 3',
-      start: new Date('January 26, 2020 08:24:00'),
-      end: new Date('February 4, 2020 12:24:00'),
-    }
-  ];
-
-  //initialstate
-
-  const initialCurrEvent = {
-    title: "",
-    start: "",
-    end: "",
-    repeat: false,
-    description: "",
-    location: "",
-  }
+  
   const [newEventModal, setNewEventModal] = useState(false);
   const [existingEventModal, setExistingEventModal] = useState(false);
   const [events, setEvents] = useState(myEvents);
@@ -67,11 +67,15 @@ const CalendarPage = () => {
     setSelectedEvent(event)
     toggleExisting(); 
   }
+  console.log(events)
     //this makes sure that currEvent isn't added to events before currEvents receives data from child
   useEffect(()=>{
-    setEvents(e => [...events,currEvent])
-    setCurrEvent(e => initialCurrEvent)
-  },[currEvent, events, initialCurrEvent])
+    if( currEvent.title !== initialCurrEvent.title){
+      let joined = events.concat(currEvent)
+      setEvents(joined)
+      setCurrEvent(initialCurrEvent)
+    }
+  },[events,currEvent])
 
   
   return (
