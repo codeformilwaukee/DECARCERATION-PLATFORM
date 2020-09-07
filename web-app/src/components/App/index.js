6670e0797cb82d6c610f1adc6ed4073a7a75aa1c
Paul Rinaldi <paulrinaldi10@yahoo.com>
2020-09-07 18:12:56 -0500

    takes correct commit from master for navigation

--- web-app/src/components/App/index.js
+++ web-app/src/components/App/index.js
@@ -4,7 +4,6 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import LandingPage from "../LandingPage";
 import SignInPage from "../SignInPage";
 import Navigation from "../Navigation";
-import HomePage from "../HomePage";
 import SignUpPage from "../SignUpPage";
 import Account from "../Account";
 import Footer from "../Footer";
@@ -22,7 +21,6 @@ const App = () => (
       <Route exact path={ROUTES.LANDING} component={LandingPage} />
       <Route exact path={ROUTES.SIGN_UP} component={SignUpPage} />
 
-      <Route exact path={ROUTES.HOME_PAGE} component={HomePage} />
       <Route exact path={ROUTES.SIGN_IN} component={SignInPage} />
 
       <Route exact path={ROUTES.ACCOUNT} component={Account} />
