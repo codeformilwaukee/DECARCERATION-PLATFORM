dac5574b3e85821495dd853d8a34dcf2f5e39958
Paul Rinaldi <rinaldi.paul1@gmail.com>
2020-10-18 22:59:02 -0500

    fix json conversion

--- web-app/src/assets/normalizeAWSJSON.py
+++ web-app/src/assets/normalizeAWSJSON.py
@@ -21,7 +21,7 @@ def normalize_sp(service_provider):
     "Attributions": service_provider["PutRequest"]["Item"]["Attributions"]["S"]
   }
 
-  print("processed: ", normalized_sp)
+  # print("processed: ", normalized_sp)
 
   return normalized_sp
 
@@ -38,16 +38,17 @@ def read_aws_json(filepath):
 
 def store_json(to_store, filepath):
   with open(filepath, 'w') as json_file:
-      json.dumps(to_store, json_file)
+    json_file.write(json.dumps(to_store, indent=4, sort_keys=True))
 
 def normalize_list_sp(raw_sp):
   normalized_service_providers = []
   for put_request in raw_sp["ServiceProvider-yoruyjsfane6zfwi54uakwddje-dev"]:
     normalized_service_providers.append(normalize_sp(put_request))
-
+  # print(len(normalized_service_providers))
   return {"service_providers": normalized_service_providers}
 
 ### MAIN ###
 all_sp_raw, all_sp_json_str = read_aws_json('shFULL.json')
 sp_json = normalize_list_sp(all_sp_raw)
+print(json.dumps(sp_json["service_providers"], indent=4, sort_keys=True))
 store_json(sp_json, 'shFULLNormalized.json')
